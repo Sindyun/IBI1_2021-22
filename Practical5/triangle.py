7d3097ce66d8ea3a7c2b76160707b2d0c3cc1c68
3210110585bit <jingyiq.21@intl.zju.edu.cn>
2022-03-16 19:19:38 +0800

    triangle

---
+++ Practical5/triangle.py
@@ -0,0 +1,23 @@
+#I have found multiple ways to calculate it.
+
+#frist define an original value of y
+y=0
+#display the first 10 values from 1 to 10. I choose "for" cycle to demostrate.
+for x in range (1,11):
+#I use the cumulation method.
+ y=y+x
+ print(y)
+
+#Also, I want to try the sum formula.
+y=0
+for x in range (1,11):
+ y=(1+x)*x/2
+ print(y)
+
+#After that, I used "while" cycle.
+y=0
+x=1
+while x < 11:
+ y=y+x
+ x=x+1
+ print(y)
