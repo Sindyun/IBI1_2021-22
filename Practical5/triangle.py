5d1cb43e9ad8321b7ec567b537e612a497c2518e
3210110585bit <jingyiq.21@intl.zju.edu.cn>
2022-05-23 23:33:07 +0800

    triangle

--- Practical5/triangle.py
+++ Practical5/triangle.py
@@ -11,13 +11,13 @@ for x in range (1,11):
 #Also, I want to try the sum formula.
 y=0
 for x in range (1,11):
- y=(1+x)*x/2
+ y=(1+x)*x/2 #use the summation formula.
  print(y)
 
 #After that, I used "while" cycle.
 y=0
 x=1
 while x < 11:
- y=y+x
+ y=y+x #the method is also cumulation.
  x=x+1
  print(y)
