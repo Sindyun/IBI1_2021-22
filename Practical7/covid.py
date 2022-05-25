797ef2e9b32a510e0b81713a29c7ac8a226721a1
3210110585bit <jingyiq.21@intl.zju.edu.cn>
2022-05-25 11:31:01 +0800

    covid

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -39,18 +39,27 @@ china_new_data.describe()
 new_cases = china_new_data.iloc[:,1]
 new_deaths = china_new_data.iloc[:,2]
 plt.boxplot(new_cases)
+plt.title("New cases")
+plt.ylabel("cases")
 plt.show()
 plt.boxplot(new_deaths)
+plt.title("New deaths")
+plt.ylabel("cases")
 plt.show()
 #plot with dates
 china_dates = china_new_data.iloc[:,0]
 plt.plot(china_dates, new_cases, 'b+')
+plt.title("China new cases")
+plt.xlabel("time")
+plt.ylabel("cases")
 plt.show()
 #plotted both new cases and new deaths in China over time
 plt.plot(china_dates, new_cases, 'ro')
 plt.plot(china_dates, new_deaths, 'b+')
 plt.xticks(china_dates.iloc[0:len(china_dates):4],rotation=-25)
 plt.title("China new cases and new deaths")
+plt.xlabel("time")
+plt.ylabel("cases")
 plt.show()
 #answer for questions:
 L1=[]
@@ -62,4 +71,19 @@ for i in range(0,7996):
  else:
     L1.append(False)
     i+=i+1
+
+#output the results
 covid_data.loc[L1,"total_deaths"]
+
+#0       0
+#1       0
+#2       0
+#3       0
+#4       0
+#       ..
+#7983    0
+#7984    0
+#7985    0
+#7986    0
+#7987    0
+#Name: total_deaths, Length: 6134, dtype: int64
